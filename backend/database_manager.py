5b3a4bd75f002e403ea481472a6c3711e3b2609b
Shubham <shbhm2023@gmail.com>
2024-03-21 07:45:34 +0530

    Generate FastAPI session for sqlalchemy

--- backend/database_manager.py
+++ backend/database_manager.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI
-from databases import Database
-from typing import Optional, Dict, Any
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+from fastapi_utils.session import FastAPISessionMaker
+from functools import lru_cache
 
 class DatabaseManager:
-    def __init__(self, database_url: str):
-        self.database_url = database_url
-        self.database = Database(database_url)
-        
-    def connect(self):
-        self.database.connect()
-        
-    def disconnect(self):
-        self.database.disconnect()
-        
-    def fetch_one(self, query: str, values: Optional[Dict[str, Any]] = None):
-        return self.database.fetch_one(query=query, values=values)
+    def __init__(self):
+        self.engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres', pool_size=100, max_overflow=120)
+        if not database_exists(self.engine.url):
+            create_database(self.engine.url)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.ModelBase = declarative_base()
+        self._cached_fastapi_sessionmaker = None
 
+    def get_db(self):
+        yield from self._get_fastapi_sessionmaker().get_db()
+
+    @lru_cache(maxsize=32)
+    def _get_fastapi_sessionmaker(self) -> FastAPISessionMaker:
+        if self._cached_fastapi_sessionmaker is None:
+            self._cached_fastapi_sessionmaker = FastAPISessionMaker(self.engine.url)
+            self._cached_fastapi_sessionmaker._cached_engine = self.engine
+        return self._cached_fastapi_sessionmaker
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,10 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, UploadFile, File, APIRouter
-from typing import Optional
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Any
-from .document_processor import DocumentProcessor
-from .database_manager import DatabaseManager
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from .api.chatbot_api import chatbot_router
 
 load_dotenv()
-
 
 origins = [
     "http://localhost",
@@ -21,17 +14,11 @@ origins = [
 
 app = FastAPI()
 
-engine = create_engine('postgresql://postgres:postgres@localhost/prostgres')
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
 )
-
-
 
 app.include_router(chatbot_router)
