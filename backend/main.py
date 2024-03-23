0d08cbdec1a4fcf0e60825cd0bb9787164a59b91
Shubham <shbhm2023@gmail.com>
2024-03-24 03:00:06 +0530

    Handle CORS issues & enabling tringle domain publicly

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from .api.chatbot_api import chatbot_router
 
 load_dotenv()
 
 origins = [
-    "http://localhost",
     "http://localhost:8080",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://tringle.zapto.org:52003/",
+    "*"
 ]
 
 app = FastAPI()
@@ -19,6 +20,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chatbot_router)
